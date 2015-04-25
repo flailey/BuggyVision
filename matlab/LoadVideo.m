81291ab77a83afb0550ee83e7c0558cc016b0f23
Shane Smith <sssmith@andrew.cmu.edu>
2015-04-25 14:50:40 -0400

    actually added gitignore files and such

--- matlab/LoadVideo.m
+++ matlab/LoadVideo.m
@@ -1,4 +1,4 @@
-function [ vid ] = LoadVideo( videoNumber )
+function [ vid ] = loadVideo( videoNumber )
 %LoadVideo loads the video run for testing
     %vid = VideoReader('../data/BuggyRun1.mov');
     vid = VideoReader(strcat('../data/BuggyRun', int2str(videoNumber), '.mov'));
