45fc8248e4b1928a52bdc6f65a6a0dd3f44559b5
Flailey <haleydalzell@gmail.com>
2015-04-25 17:06:31 -0400

    homography :D

--- matlab/testFlat.m
+++ matlab/testFlat.m
@@ -1,7 +1,11 @@
-vid = loadVideo(1);
+vid = loadVideo(2);
 
-frame = read(vid,1);
+frame = read(vid,3540);
+
+figure(100);
+imagesc(frame);
 
 wFrame = transformToFlat(frame);
 
+figure(101);
 imshow(wFrame);
--- matlab/transformToFlat.m
+++ matlab/transformToFlat.m
@@ -2,15 +2,32 @@ function [ flatImage ] = transformToFlat( cameraImage )
 %transformToFlat transforms a perspective camera image to a flat, top-down
 %view of the scene
 
-    tilt = deg2rad(20);
+    tilt = -1* deg2rad(.25);
     
-    warpSize = [600, 600];
+    warpSize = [2000,2000];
+    %warpSize = [1000, 1000];
+    
+    C = eye(3);
+    H = eye(3);
+    c2 = eye(3);
+    
+    
+    c2 = [1, 0,-400;
+         0, 1,-320;
+         0, 0,1];
+    
+    %H = [cos(tilt),-sin(tilt),0;
+    %      sin(tilt),cos(tilt),0;
+    %      0,0,1];
+    H = [1,0,0;
+         0,cos(tilt),sin(tilt);
+         0,-sin(tilt),cos(tilt)];
+    
+    C = [1,0,400 + 500;
+          0,1,320 + 1200;
+          0,0,1];
 
-    H = [1,0,0; ...
-         0,sin(tilt),-sin(tilt); ...
-         0,cos(tilt),cos(tilt)];
-
-    flatImage = warpH(cameraImage, H, warpSize ,0);
+    flatImage = warpH(cameraImage, C*H*c2, warpSize ,0);
 
 end
 
