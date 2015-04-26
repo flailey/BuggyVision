c1f578e380a9e896326f960608d4a5989eb7fdcf
Shane Smith <sssmith@andrew.cmu.edu>
2015-04-26 13:41:15 -0400

    shane's stuff

--- matlab/testMosaicShane.m
+++ matlab/testMosaicShane.m
@@ -1,6 +1,6 @@
 vid = loadVideo(2);
 
-fNum = 1400; % 1300
+fNum = 1350; % 1300
 
 frame1 = read(vid,fNum);
 
@@ -17,19 +17,24 @@ imwrite(wFrame1,'flat1.png');
 frame2 = read(vid,fNum + 2);
 wFrame2 = transformToFlat(frame2);
 
-wFrame1 = imresize(wFrame1, 0.3);
-wFrame2 = imresize(wFrame2, 0.3);
+frame1 = imresize(frame1, 0.1);
+frame2 = imresize(frame2, 0.1);
 
-%figure(101);
-I = rgb2gray(wFrame1);
+%wFrame1 = imresize(wFrame1, 0.5);
+%wFrame2 = imresize(wFrame2, 0.5);
+
+figure(101);
+I = rgb2gray(frame1);
 %points = detectBRISKFeatures(I);
 %corners = detectFASTFeatures(I);
 %regions = detectMSERFeatures(I);
-points = detectSURFFeatures(I);
+%points = detectSURFFeatures(I);
+%points = detectHarrisFeatures(I);
+points = detectMinEigenFeatures(I);
 %[f1, vpts1, hogVisualization] = extractHOGFeatures(wFrame1, regions);
 [f1, vpts1] = extractFeatures(I, points);
-imshow(wFrame1); hold on;
-plot(points.selectStrongest(40));
+imshow(frame1); hold on;
+plot(points.selectStrongest(1000));
 %plot(corners.selectStrongest(40));
 %plot(regions, 'showPixelList', true, 'showEllipses', false);
 %plot(points.selectStrongest(10));
@@ -38,16 +43,18 @@ plot(points.selectStrongest(40));
 
 
 figure(102);
-I2 = rgb2gray(wFrame2);
-%points = detectBRISKFeatures(I2);
-%corners = detectFASTFeatures(I2);
+I2 = rgb2gray(frame2);
+%points2 = detectBRISKFeatures(I2);
+%points2 = detectHarrisFeatures(I);
+points2 = detectMinEigenFeatures(I);
+%corners2 = detectFASTFeatures(I2);
 %regions2 = detectMSERFeatures(I2);
-points2 = detectSURFFeatures(I2);
+%points2 = detectSURFFeatures(I2);
 %imshow(wFrame2); hold on;
 %[f2, vpts2, hogVisualization] = extractHOGFeatures(wFrame2, regions2);
 [f2, vpts2] = extractFeatures(I2, points2);
-imshow(wFrame2); hold on;
-plot(points2.selectStrongest(40));
+imshow(frame2); hold on;
+plot(points2.selectStrongest(1000));
 %plot(corners.selectStrongest(40));
 %plot(points.selectStrongest(10));
 %figure(101);
@@ -61,15 +68,15 @@ matchedPoints2 = vpts2(indexPairs(:, 2));
 
 
 figure(103); ax = axes;
-showMatchedFeatures(wFrame1, wFrame2,matchedPoints1,matchedPoints2,'montage','Parent',ax);
+showMatchedFeatures(frame1, frame2,matchedPoints1,matchedPoints2,'montage','Parent',ax);
 title(ax, 'Candidate point matches');
 legend(ax, 'Matched points 1','Matched points 2');
 
-figure(101);
-imshow(wFrame1);
+%figure(101);
+%imshow(wFrame1);
 
-figure(102);
-imshow(wFrame2);
+%figure(102);
+%imshow(wFrame2);
 
 
 %figure(103);
