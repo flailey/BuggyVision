9edf3107a8175cf69b92f02cc4c4b4a36f93a241
Flailey <haleydalzell@gmail.com>
2015-04-27 13:16:30 -0400

    load video bullcrap

--- matlab/LoadVideo.m
+++ matlab/LoadVideo.m
@@ -1,7 +1,11 @@
 function [ vid ] = loadVideo( videoNumber )
 %LoadVideo loads the video run for testing
     %vid = VideoReader('../data/BuggyRun1.mov');
-    vid = VideoReader(strcat('../data/large/Icarus.mp4'));
+    if videoNumber == 2
+        vid = VideoReader(strcat('../data/large/Icarus.mp4'));
+    elseif videoNumber == 3 
+        vid = VideoReader(strcat('../data/large/mobot1.mov'));  
+    end
     %vid = VideoReader(strcat('../data/Ascension', int2str(videoNumber), '.mp4'));
     %numFrames = vid.NumberOfFrames;
     %frames = read(vid,i);
