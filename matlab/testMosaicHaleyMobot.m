7f9ed392651efe1c7b8096b6c2e61289f33c0b4c
Flailey <haleydalzell@gmail.com>
2015-04-27 16:55:53 -0400

    made changes to transformToFlatMobot function

--- matlab/testMosaicHaleyMobot.m
+++ matlab/testMosaicHaleyMobot.m
@@ -1,26 +1,48 @@
 vid = loadVideo(3);
-fNum = 200;
+fNum = 50;
 
 % constants
-resizeto = 0.1;
+resizeto = .6;
+tilt = -1 * degtorad(0.05) / resizeto;
 
 % load, mask, and transform frame 1
 frame1 = read(vid,fNum);
-wFrame1 = transformToFlatMobot(frame1);
+frame1 = imresize(frame1, resizeto);
+wFrame1 = transformToFlatMobot(frame1, tilt, 0);
 
-%figure(100);
-%imagesc(frame1);
+figure(100);
+imagesc(frame1);
 
 % load, mask, and transform frame 2
-frame2 = read(vid,fNum + 2);
-wFrame2 = transformToFlatMobot(frame2);
+frame2 = read(vid,fNum + 5);
+frame2 = imresize(frame2, resizeto);
+wFrame2 = transformToFlatMobot(frame2, tilt, 0);
 
 Im1 = wFrame1;
 Im2 = wFrame2;
 
-% display the frames
-figure(10);
-imagesc(Im1);
-figure(11);
-imagesc(Im2);
+%%
+figure(101);
+I1 = rgb2gray(Im1);
+points = detectBRISKFeatures(I1);
+[f1, vpts1] = extractFeatures(I1, points);
+imshow(Im1); hold on;
+plot(points.selectStrongest(1000));
 
+figure(102);
+I2 = rgb2gray(Im2);
+points2 = detectBRISKFeatures(I2);
+[f2, vpts2] = extractFeatures(I2, points2);
+imshow(Im2); hold on;
+plot(points2.selectStrongest(1000));
+
+%%
+indexPairs = matchFeatures(f1, f2) ;
+matchedPoints1 = vpts1(indexPairs(:, 1));
+matchedPoints2 = vpts2(indexPairs(:, 2));
+
+%%
+figure(103);
+showMatchedFeatures(Im1,Im2,matchedPoints1,matchedPoints2,'montage');
+title(ax, 'Candidate point matches');
+legend(ax, 'Matched points 1','Matched points 2');
--- matlab/transformToFlatMobot.m
+++ matlab/transformToFlatMobot.m
@@ -1,15 +1,15 @@
-function [ flatImage ] = transformToFlatMobot( cameraImage )
+function [ flatImage ] = transformToFlatMobot( cameraImage, tilt, pan )
 %transformToFlat transforms a perspective camera image to a flat, top-down
 %view of the scene
 
-    tilt = -1* deg2rad(.055);%.245);
-    %tilt = 0;
-    pan = 1* deg2rad(3.5);
+    %tilt = -1* deg2rad(.055); %.245);
+    %tilt = -1* deg2rad(.5);
+    pan = 0; %1* deg2rad(3.5);
     
     width = size(cameraImage,2);
     height = size(cameraImage,1);
     
-    warpSize = [height*4,width*4];
+    warpSize = [height*3,width*3];
     %warpSize = [1000, 1000];
     
     c2 = [1, 0,-width/2;
@@ -26,8 +26,8 @@ function [ flatImage ] = transformToFlatMobot( cameraImage )
             -sin(pan), cos(pan), 0;
                     0,        0, 1];
     
-    C = [  1,  0, width*2;
-           0,  1, height*4;
+    C = [  1,  0, width*1.5;
+           0,  1, height*3;
            0,  0,  1];
 
     flatImage = warpH(cameraImage, C*panH*tiltH*c2, warpSize ,0);
